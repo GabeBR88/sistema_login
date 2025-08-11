11bf5653b0de9784ea3febb42d93f303fdb9e7d9
Gabriel Brito <gabrielbrito.contato2017@gmail.com>
2025-08-11 14:42:06 -0300

    Criando a classe Usuario

---
+++ usuario.py
@@ -0,0 +1,25 @@
+import hashlib
+
+class Usuario:
+    def __init__(self, nome:str, email:str):
+        self.nome = nome
+        self.email = email
+        self.senha_hash = None
+
+    
+    def set_senha(self, senha_texto):
+        self.senha_hash = hashlib.sha256(senha_texto.encode()).hexdigest()
+
+
+    def verificar_senha(self, senha_texto):
+        return hashlib.sha256(senha_texto.encode()).hexdigest() == self.senha_hash
+
+
+teste = Usuario("Gabriel", "teste@gmail.com")
+teste.set_senha("TestandoSenha*22")
+
+print(teste.verificar_senha("testando"))
+print(teste.verificar_senha("TestandoSenha*22"))
+
+print(hasattr(teste, "senha"))
+print(teste.senha_hash)
