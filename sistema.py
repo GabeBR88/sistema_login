2dc34755b1d694f598cb8f133eb5a2648bd69d02
Gabriel Brito <gabrielbrito.contato2017@gmail.com>
2025-08-13 18:46:59 -0300

    Criação da classe SistemaUsuarios

---
+++ sistema.py
@@ -0,0 +1,78 @@
+from usuario import *
+from utils import *
+from pathlib import Path
+import re
+
+path = Path(r"E:\Workspace\Um treino por dia\AGOSTO\Projeto1\sistema_login\data\cadastro.json")
+
+class SistemaUsuarios:
+    def __init__(self):
+        self.lista_usuarios = []
+
+    
+    def validar_email(self, email: str) -> bool:
+        return re.match(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$", email) is not None
+
+    def validar_senha(self, senha: str) -> bool:
+        return re.match(r"^.{6,}$", senha) is not None
+    
+
+    def cadastrar_usuario(self, usuario:Usuario):
+        self.lista_usuarios.extend(carregar_json(path))
+
+        if not self.validar_email(usuario.email):
+            print("❌ E-mail inválido. Tente novamente!")
+            return
+            
+        senha = input("Digite a senha: ")
+        if not self.validar_senha(senha):
+            print("❌ A senha deve ter pelo menos 6 caracteres.")
+            return
+            
+        usuario.set_senha(senha)
+
+        if usuario.verificar_senha(senha) == True:
+            dados = {
+                "nome" : usuario.nome,
+                "email" : usuario.email,
+                "senha" : usuario.senha_hash
+            }
+
+            self.lista_usuarios.append(dados)
+            salvar_json(path, self.lista_usuarios)
+            print("Salvo com sucesso!")
+        
+        else:
+            print("Tente novamente!")
+
+
+    def autenticar_usuario(self, email:str, senha:str):
+        usuarios = carregar_json(path)
+        if not usuarios:
+            print("Não há dados cadastrados!")
+            return
+
+        for usuario in usuarios:
+            if usuario['email'] == email:
+                senha_hash = hashlib.sha256(senha.encode()).hexdigest()
+                if usuario['senha'] == senha_hash:
+                        print(f"Nome: {usuario['nome']}")
+                        break
+                else:
+                    print("Nenhum usuario localizado com a senha informada!")
+            else:
+                print("Nenhum usuario localizado com o e-mail informado!")
+            
+
+    def listar_usuarios(self):
+        usuarios = carregar_json(path)
+        if not usuarios:
+            print("Não há dados cadastrados!")
+            return
+        
+        for usuario in usuarios:
+            print(f"Nome: {usuario['nome']}")
+            print(f"E-mail: {usuario['email']}\n")
+           
+
+
